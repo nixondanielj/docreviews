b961ddacfed5100300d7e6531d5d1d5434119d97
Dan Nixon <nixon.daniel.j@gmail.com>
2015-01-13 04:08:26 +0000

    auth works for both web reqs and sockets

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var sessionStore = new MongoStore({ mongooseConnection: mongoose.connection });
 var app = express();
 var server = http.createServer(app);
 
-var key = 'express.dans-key', 
+var key = 'mykey', 
     secret = 'fakeSecret',
     GOOGLE_CLIENT_ID = '33815653519-jh94hqfkkumbgdsrb05ck1eoiea7kf8u.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET = '2DtFN21fZsU4ph4rBTvvzxAU';
@@ -27,21 +27,32 @@ passport.use(new GoogleStrategy({
         clientSecret: GOOGLE_CLIENT_SECRET,
         callbackURL: 'https://mymean-nixondanielj.c9.io/auth/google'
     },
-    function(identifier, profile, done){
+    function(accessToken, refreshToken, profile, done){
         // find user here
-        done(false, { id: identifier });
+        done(null, profile);
     })
 );
+
+passport.serializeUser(function(user, callback){
+        console.log('serializing user.');
+        callback(null, user);
+    });
+
+passport.deserializeUser(function(user, callback){
+       console.log('deserialize user.');
+       callback(null, user);
+    });
 
 app.use(session({
     key: key,
     secret: secret,
     store: sessionStore
 }));
-
 app.use(express.static(__dirname + '/client'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/auth/google', passport.authenticate('google', { scope: 'email' }));
+app.get('/auth/google/init', passport.authenticate('google', { scope: 'email' }));
 
 app.get('/auth/google', 
     passport.authenticate('google', {failureRedirect: '/#/authFail'}),
@@ -49,9 +60,16 @@ app.get('/auth/google',
         console.log('successful login');
         res.redirect('/');
     });
-
+app.get('/status', function(req, res){
+    if(req.isAuthenticated()){
+        res.send('thanks bro');
+    } else {
+        res.sendStatus(401);
+    }
+});
 // socket io setup
 var io = require('socket.io')(server);
+var sockets = [];
 var socketPassport = require('passport.socketio');
 io.set('authorization', socketPassport.authorize({
     cookieParser: cookieParser,
@@ -60,17 +78,31 @@ io.set('authorization', socketPassport.authorize({
     store: sessionStore,
     success: function(data, accept){
         console.log('Socket connection authenticated');
-        accept();
+        accept(null, true);
     },
     error: function(data, message, error, accept){
+        console.log('Socket connection authentication failed');
         console.log(message);
+        
         // if there was a real error
-        if(error) 
+        if(error) {
             accept(new Error(message));
+        }
+        
         // if user was unauthorized
         accept(null, false);
     }
 }));
+
+io.on('connection', function(socket){
+    console.log('Added a new socket');
+    sockets.push(socket);
+    socket.on('test', function(){
+        if(socket.request.user && socket.request.user.logged_in){
+            socket.emit('authed');
+        }
+    });
+});
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
